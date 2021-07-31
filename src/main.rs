064b91299615c89697226d2a4ac3375efb4a065e
CDinneya <charles.dinneya@stanbicibtc.com>
2021-07-31 19:57:08 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,27 @@
 use std::fs::*;
 use std::path::Path;
 use serde::{Deserialize, Serialize};
-use std::ffi::OsString;
-use std::io::Error;
-use std::process::Command as command;
+use std::process::Command;
 
 fn main() {
     let contents = read_json("file.json");
 
     let current_directory = std::env::current_dir().expect("error accessing directory");
 
-    let files = read_dir(current_directory);
-
     let build: Build = serde_json::from_str(&contents).unwrap();
-
-    let list_directory = command::new("ls").output().expect("Error executing command");
 
     for command in build.builds.iter() {
         // let e =  std::str::from_utf8(list_directory.stdout.as_slice()).unwrap().to_string().contains(command.build_file);
+        let build_file = &command.build_file;
+
+        read_dir(&current_directory).unwrap().for_each(|f| {
+            let file = f.unwrap().file_name().into_string().unwrap();
+            if file.eq(build_file) {
+                // Command::new(&command.tasks.test.to_string()).output().unwrap();
+            }
+        });
     }
-
-
+}
 
 fn read_json(path: &str) -> String {
     let file = File::open(Path::new(path));
@@ -39,6 +40,7 @@ fn read_json(path: &str) -> String {
 
     return contents;
 }
+
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Tasks {

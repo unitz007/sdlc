c414318b4b2cb26b04348a9dec8011d63de5911a
CDinneya <charles.dinneya@stanbicibtc.com>
2021-08-31 21:53:51 +0100

    add command execution

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,65 @@
 use std::fs::*;
 use std::path::Path;
 use serde::{Deserialize, Serialize};
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::io::{Error, BufReader, ErrorKind, BufRead};
+// use std::io::{_Write};
 
-fn main() {
+
+fn main()-> Result<(), Error> {
     let contents = read_json("file.json");
 
     let current_directory = std::env::current_dir().expect("error accessing directory");
 
+    // println!("{:?}", current_directory);
+
     let build: Build = serde_json::from_str(&contents).unwrap();
 
+    let mut program = "";
+    let mut com = "";
+
+
     for command in build.builds.iter() {
-        // let e =  std::str::from_utf8(list_directory.stdout.as_slice()).unwrap().to_string().contains(command.build_file);
         let build_file = &command.build_file;
 
-        read_dir(&current_directory).unwrap().for_each(|f| {
+
+
+        for f in read_dir(&current_directory).unwrap().into_iter() {
             let file = f.unwrap().file_name().into_string().unwrap();
             if file.eq(build_file) {
-                // Command::new(&command.tasks.test.to_string()).output().unwrap();
+                // break;
+                // println!("{}", build_file);
+                program = &command.tasks.program;
+                com = &command.tasks.run;
             }
-        });
+
+        }
     }
+
+    let output = Command::new(program.to_string())
+        .args(&[com.to_string()])
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+
+    let reader = BufReader::new(output);
+
+    reader.lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("{}", line));
+
+    Ok(())
+
 }
 
+// print!("{}", program);
+
+
 fn read_json(path: &str) -> String {
+
+    // println!("{:?}", Path::new(path));
     let file = File::open(Path::new(path));
     let contents;
 
@@ -44,19 +80,20 @@ fn read_json(path: &str) -> String {
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Tasks {
-    run : String,
+    program: String,
+    run: String,
     test: String,
-    build: String
+    build: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Commands {
     build_file: String,
-    tasks: Tasks
+    tasks: Tasks,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Build {
-    builds: Vec<Commands>
+    builds: Vec<Commands>,
 }
 

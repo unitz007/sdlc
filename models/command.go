f98215c3f9fc64bcd34f60754186e79c6f2adba1
charlesDinneya <dinneyacharles007@gmail.com>
2023-05-14 19:44:56 +0200

    Test_commandStructShouldImplementCommandSpec passed

--- models/command.go
+++ models/command.go
@@ -1,1 +1,26 @@
 package models
+
+type command struct {
+	buildFile string
+	task      TaskSpec
+}
+
+type CommandSpec interface {
+	BuildFile() string
+	Task() TaskSpec
+}
+
+func NewCommand(buildFile string, task TaskSpec) CommandSpec {
+	return command{
+		buildFile: buildFile,
+		task:      task,
+	}
+}
+
+func (c command) BuildFile() string {
+	return c.buildFile
+}
+
+func (c command) Task() TaskSpec {
+	return c.task
+}
